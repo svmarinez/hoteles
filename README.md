0795c82aafb73b62304313da5e163923b55e9df7
Sara Vasquez <30931982+svmarinez@users.noreply.github.com>
2022-04-04 01:01:03 +0200

    README added

---
+++ README.md
@@ -0,0 +1,17 @@
+###Desarrollar una aplicación para la gestión de hoteles.
+
+Para ello se debe de crear una tabla hoteles con los campos:
+
+-Nombre
+-Localidad
+-Provincia
+-Precio_noche
+-Num_habitaciones
+-Habitaciones_disponibles
+
+Se debe de implementar una aplicación que de de alta los hoteles en esa base de datos y que realice las operaciones de:
+
+-Listado de hoteles
+-Busqueda de hoteles por localidad o provincia y nos devuelva la lista de hoteles ordenada por precio_noche.
+
+** _Para este ejercicio usar PDO._ **
--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
   declare(strict_types = 1);
-  include '/includes/autoloader.inc.php'
+  include 'includes/autoloader.inc.php'
  ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -13,19 +14,12 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;400&family=Raleway:wght@100;300&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
- Save
-    <link rel="stylesheet" href="./styles/style.css">
+ <link rel='stylesheet' type='text/css' href='./styles/styles.php' />
     <title>Hoteles!</title>
 </head>
 <body>
-    <h1>Hotels</h1>
-    <h2>hello world!</h2>
-    <h3>hello world!</h3>
-    <h4>hello world!</h4>
-    <h5>hello world!</h5>
-    <h6>hello world!</h6>
-    <p>hello world!</p>
+    <h1 class="text-center m-4 p-4">Hotels</h1>
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
- Save
 </body>
 </html>
