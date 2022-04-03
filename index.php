df9fe33e6926f3455d0ceaed428bb7ee9d5dd790
Sara Vasquez <30931982+svmarinez@users.noreply.github.com>
2022-04-03 23:57:57 +0200

    project initialized

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+  declare(strict_types = 1);
+  include '/includes/autoloader.inc.php'
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;400&family=Raleway:wght@100;300&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+ Save
+    <link rel="stylesheet" href="./styles/style.css">
+    <title>Hoteles!</title>
+</head>
+<body>
+    <h1>Hotels</h1>
+    <h2>hello world!</h2>
+    <h3>hello world!</h3>
+    <h4>hello world!</h4>
+    <h5>hello world!</h5>
+    <h6>hello world!</h6>
+    <p>hello world!</p>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+ Save
+</body>
+</html>
